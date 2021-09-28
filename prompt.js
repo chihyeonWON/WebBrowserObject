7ee8d81d555c2f6e065dd04f8447c015a672a7ee
원치현 <heungeob1003@gmail.com>
2021-09-28 19:38:01 +0900

    prompt execrises

---
+++ prompt.js
@@ -0,0 +1,6 @@
+//prompt의 반환값을 모두 문자열
+var name = prompt("이름을 입력하십시오");
+var name = prompt("이름을 입력하십시오","이현규");
+//prompt로 숫자 등을 활용하려면 parseInt,parseFloat 메서드를 사용한다.
+var age = parseInt(prompt("나이를 입력하십시오")); 
+var height = parseFloat(prompt("키를 입력하십시오"));
