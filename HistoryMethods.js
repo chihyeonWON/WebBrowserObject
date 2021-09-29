1b2675509683455cbb75733b63fac17abe7b08bd
원치현 <heungeob1003@gmail.com>
2021-09-29 19:07:38 +0900

    History Object Methods

---
+++ HistoryMethods.js
@@ -0,0 +1,5 @@
+history.back(); // 웹 페이지 열람 이력을 하나 되돌아간다
+history.forward(); // 웹 페이지 열람 이력을 하나 진행한다
+history.go(-3); // 웹 페이지 열람 이력을 세 개 되돌아간다
+history.go(2); // 웹 페이지 열람 이력을 두 개 진행한다
+history.pushState(null,null,"page2.html");  
